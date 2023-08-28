96c4f6032ac2bd167d4d2beeac79e82b261a841c
Yaroslava Pletsys <103317047+Edouena8@users.noreply.github.com>
2023-08-28 16:25:49 +0200

    corrected schema

--- models/contact.js
+++ models/contact.js
@@ -1,5 +1,10 @@
 const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 const { handleMongooseError } = require("../helpers");
+
+const relationships = ["famile", "work", "friend", "service"];
+const dateRegExp =
+  /(0?[1-9]|[12][0-9]|3[01])[- _](January|February|March|Aprel|May|June|July|August|September|October|November|December)[- _]((19|20)\d\d)/;
 
 const contactSchema = new Schema(
   {
@@ -9,13 +14,25 @@ const contactSchema = new Schema(
     },
     email: {
       type: String,
+      required: [true, "Set email for contact"],
     },
     phone: {
       type: String,
+      min: 3,
+      max: 20,
+      required: [true, "Set phone for contact"],
     },
     favorite: {
       type: Boolean,
       default: false,
+    },
+    dateOfBirth: {
+      type: String,
+      match: dateRegExp,
+    },
+    relationship: {
+      type: String,
+      enum: relationships,
     },
   },
   {
@@ -26,8 +43,27 @@ const contactSchema = new Schema(
 
 contactSchema.post("save", handleMongooseError);
 
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().min(3).max(20).required(),
+  favorite: Joi.boolean,
+  dateOfBirth: Joi.string().pattern(dateRegExp),
+  relationship: Joi.string().valid(...relationships),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
+
 const Contact = model("contact", contactSchema);
 
 module.exports = {
   Contact,
+  schemas,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,27 +3,27 @@ const ctrl = require("../../controllers/contacts");
 const router = express.Router();
 
 const { validateBody, isValidId } = require("../../middlewares");
-const schema = require("../../schemas/contacts");
+const { schemas } = require("../../models/contact");
 
 router.get("/", ctrl.listContacts);
 
 router.get("/:contactId", isValidId, ctrl.getContactById);
 
-router.post("/", validateBody(schema.addSchema), ctrl.addContact);
+router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
 router.delete("/:contactId", isValidId, ctrl.removeContact);
 
 router.put(
   "/:contactId",
   isValidId,
-  validateBody(schema.addSchema),
+  validateBody(schemas.addSchema),
   ctrl.updateContact
 );
 
 router.patch(
   "/:contactId/favorite",
   isValidId,
-  validateBody(schema.updateFavoriteSchema),
+  validateBody(schemas.updateFavoriteSchema),
   ctrl.updateFavorite
 );
 
