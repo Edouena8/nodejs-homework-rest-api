7ecd6f81ac702d57e97de680e1fcc43f76167dce
Yaroslava Pletsys <103317047+Edouena8@users.noreply.github.com>
2023-08-28 15:20:12 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
-const crypto = require("node:crypto");
+const crypto = require("crypto");
 
 const contactsPath = path.join(__dirname, "./contacts.json");
 
