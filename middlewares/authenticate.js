21fda046d3139a768c2b0f7b0c3507c663c48abb
Yaroslava Pletsys <103317047+Edouena8@users.noreply.github.com>
2023-09-05 12:48:06 +0200

    corrected authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,19 +8,19 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
-    next(HttpError(401, "bearer"));
+    next(HttpError(401));
   }
 
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user) {
-      next(HttpError(401, "user"));
+    if (!user || !user.token || user.token !== token) {
+      next(HttpError(401));
     }
     req.user = user;
     next();
   } catch {
-    next(HttpError(401, "catch"));
+    next(HttpError(401));
   }
 };
 
